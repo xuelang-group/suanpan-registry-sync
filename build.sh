4e7899272e18b6c3fdcb4ff266fe05e0364241c6
YanQinghao <woshiyanqinghao@gmail.com>
2022-03-16 13:23:17 +0800

    fix

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GITHUBTOKEN=$1
+IMAGES=$2
+
+curl --location --request POST 'https://api.github.com/repos/xuelang-group/suanpan-registry-sync/dispatches' \
+--header 'Accept: application/vnd.github.v3+json' \
+--header 'Authorization: token '$GITHUBTOKEN'' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "event_type": "sync_image",
+    "client_payload": {
+        "registry": "registry.cn-shanghai.aliyuncs.com",
+        "namespace": "shuzhi-amd64",
+        "names": '$IMAGES'
+    }
+}'
--- sync.sh
+++ sync.sh
@@ -2,12 +2,15 @@
 
 REGISTRY=$1
 NAMESPACE=$2
-NAME=$3
+NAMES=$3
 TOREGISTRY=$4
+IFS=',' read -ra NAMEARR <<< "$NAMES"
 
-echo "start to pull image: $REGISTRY/$NAMESPACE/$NAME"
-docker pull "$REGISTRY/$NAMESPACE/$NAME"
-echo "tag image to: $TOREGISTRY/$NAMESPACE/$NAME"
-docker tag "$REGISTRY/$NAMESPACE/$NAME" "$TOREGISTRY/$NAMESPACE/$NAME"
-echo "start to push image: $TOREGISTRY/$NAMESPACE/$NAME"
-docker push "$TOREGISTRY/$NAMESPACE/$NAME"
+for NAME in "${NAMEARR[@]}"; do
+    echo "start to pull image: $REGISTRY/$NAMESPACE/$NAME"
+    docker pull "$REGISTRY/$NAMESPACE/$NAME"
+    echo "tag image to: $TOREGISTRY/$NAMESPACE/$NAME"
+    docker tag "$REGISTRY/$NAMESPACE/$NAME" "$TOREGISTRY/$NAMESPACE/$NAME"
+    echo "start to push image: $TOREGISTRY/$NAMESPACE/$NAME"
+    docker push "$TOREGISTRY/$NAMESPACE/$NAME"
+done
